b2fce67e312846132f2ac3d623929beb1736b7ed
dimka <e.atagazina@pravo.tech>
2023-04-15 18:10:24 +0400

    first commit

--- zadacha19.py
+++ zadacha19.py
@@ -1,5 +1,10 @@
 import random
 lst = []
 for i in range(10):
-    lst.append(randint(1,100))
+    lst.append(random.randint(1,100))
 print(lst)
+N = int(input('введите число'))
+for i in range(N):
+    a = lst.pop()
+    lst.insert(0, a)
+    print(lst)
